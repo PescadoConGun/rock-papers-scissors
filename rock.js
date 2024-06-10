ba71eedf33844b302fcee34d7568294626e30a24
Pescado <dzargm@gmail.com>
2024-06-10 19:45:02 +0200

    Added getComputerChoice function

---
+++ rock.js
@@ -0,0 +1,9 @@
+console.log("Hello!")
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)];
+    console.log(choice);
+    }
+getComputerChoice();
