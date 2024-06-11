f3ebe03bd747bb04958d2a4de20a7b160c8dcf85
Pescado <dzargm@gmail.com>
2024-06-11 21:45:04 +0200

    Revert "Added getComputerChoice function"
    This reverts commit ba71eedf33844b302fcee34d7568294626e30a24.

--- rock.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello!")
-
-const options = ["rock", "paper", "scissors"];
-
-function getComputerChoice() {
-    const choice = options[Math.floor(Math.random() * options.length)];
-    console.log(choice);
-    }
-getComputerChoice();
